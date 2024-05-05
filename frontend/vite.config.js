0ced30d55ae4948a761c68b82e39558b1278dd6c
Santosh <santoshkarki9100@gmail.com>
2024-05-05 12:30:40 +1000

    deploy commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/reservation': 'http://localhost:4000',
-      '/user': 'http://localhost:4000'
+      '/reservation': 'https://restaurant-zsuu.onrender.com',
+      '/user': 'https://restaurant-zsuu.onrender.com`'
     }
   }
 })
