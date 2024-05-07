76ec2d13da46b7887039cae6b6776fd9cd6583ae
Santosh <santoshkarki9100@gmail.com>
2024-05-07 13:08:39 +1000

    final deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/reservation': ['http://localhost:4000', 'https://restaurant-o7bo.onrender.com'],
-      '/user': ['http://localhost:4000', 'https://restaurant-o7bo.onrender.com']
+      '/reservation': 'https://restaurant-o7bo.onrender.com',
+      '/user': 'https://restaurant-o7bo.onrender.com'
     }
   }
 })
